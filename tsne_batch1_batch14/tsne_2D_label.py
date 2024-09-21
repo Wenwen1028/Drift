57151c44b7a3b0f8981ebccbb5b0cc6b1d750729
Wenwen1028 <wenwen.zhang@ntu.edu.sg>
2024-09-21 21:29:38 +0800

    Add files via upload

--- tsne_batch1_batch14/tsne_2D_label.py
+++ tsne_batch1_batch14/tsne_2D_label.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
+matplotlib.use('Agg')
+from sklearn.manifold import TSNE
 import pandas as pd
 
 src_names = ["batch1", "batch2", "batch3", "batch4", "batch5", "batch6", "batch7", "batch8", "batch9", "batch10", "batch11", "batch12", "batch13", "batch14"]
@@ -26,28 +26,33 @@ def loadLabels(fileName):
     labels = np.array(labels)
     return labels
 
-def _PCA():
+def _TSNE():
     for src_name in src_names:
         data_path = f'../Dataset/Dataset_ext/{src_name}_ext.txt'
         label_path = f'../Dataset/Dataset_lab/{src_name}_lab.txt'
+
         data = loadDataSet(data_path)
         labels = loadLabels(label_path)
-        pca = PCA(n_components=2)
-        pca_result = pca.fit_transform(data)
+
+        tsne = TSNE(n_components=2, random_state=42)
+        tsne_result = tsne.fit_transform(data)
+
         plt.figure()
-        scatter = plt.scatter(pca_result[:, 0], pca_result[:, 1], c=labels, cmap='viridis', marker='o')
-        plt.title(f'{src_name}_PCA')
+        scatter = plt.scatter(tsne_result[:, 0], tsne_result[:, 1], c=labels, cmap='viridis', marker='o')
+        plt.title(f'{src_name}_tSNE')
         plt.legend(handles=scatter.legend_elements()[0], labels=set(labels))
-        plt.savefig(f'./pca_plots/{src_name}_pca.png')
+        plt.savefig(f'./tsne_plots/{src_name}_tsne.png')
         plt.show()
 
-        df = pd.DataFrame(data=pca_result, columns=['PCA1', 'PCA2'])
+        # Save to Excel
+        df = pd.DataFrame(data=tsne_result, columns=['tSNE1', 'tSNE2'])
         df['label'] = labels
-        df.to_excel(f'./pca_plots/{src_name}_pca.xlsx', index=False)
-        print(f'{src_name} PCA data and labels saved to Excel.')
+        df.to_excel(f'./tsne_plots/{src_name}_tsne.xlsx', index=False)
+        print(f'{src_name} tSNE data and labels saved to Excel.')
 
 if __name__ == '__main__':
-    _PCA()
+    _TSNE()
+
 
 
 
